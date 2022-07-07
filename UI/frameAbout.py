2457bcf8c25779c77ad8a2a2dcdb1a3826929396
Din Ezra <dinezra11@gmail.com>
2022-07-07 13:33:45 +0300

    About Us Page

--- UI/frameAbout.py
+++ UI/frameAbout.py
@@ -5,16 +5,27 @@
               Lior Swissa   318657384
 """
 import tkinter as tk
+import webbrowser
 
 
 def teamInfoFrame(root):
     """ Create the development team's information section. """
+
+    def dinLinkedIn():
+        webbrowser.open("https://www.linkedin.com/in/din-ezra-05abb4216/")
+
+    def liorLinkedIn():
+        webbrowser.open("https://www.linkedin.com/in/lior-swissa-752884240/")
+
     frame = tk.Frame(root)
 
     # Initialize the frame's properties
     tk.Label(frame, text="Development Team:").grid(column=0, row=0, padx=(0, 10))
     tk.Label(frame, text="Din Ezra (dinezra11@gmail.com)").grid(column=1, row=0, sticky="w")
     tk.Label(frame, text="Lior Swissa (Liorsw5@gmail.com)").grid(column=1, row=1, sticky="w")
+
+    tk.Button(frame, text="Linkedin", command=dinLinkedIn).grid(column=2, row=0, padx=(10, 0))
+    tk.Button(frame, text="Linkedin", command=liorLinkedIn).grid(column=2, row=1, padx=(10, 0))
 
     return frame
 
@@ -42,12 +53,15 @@ def getFrame(window, navigateFunction):
     frame = tk.Frame(window)
 
     # Initialize the frame's widgets
-    tk.Label(frame, text="About Us", font=(None, 24)).pack(pady=10)
-    modelsInfoFrame(frame).pack(anchor="w")
-    tk.Label(frame,
-             text="The project include our own implementations for the Naive Bayes and ID3 algorithms, and alternative implementations from sklearn library.").pack(
-        anchor="w")
-    teamInfoFrame(frame).pack(anchor="w", pady=(20, 0))
+    tk.Label(frame, text="About Us", font=(None, 24)).pack()
+    tk.Label(frame, text='This project is part of the course "Intro to Data Mining" in SCE.').pack(anchor="w")
+    tk.Label(frame, text="The project is a classifier application, which can load a dataset, perform pre-processing "
+                         "to it and create a machine learning model from it.").pack(anchor="w")
+    tk.Label(frame, text="It also tests the new model and provide the accuracy measurements.").pack(anchor="w")
+    modelsInfoFrame(frame).pack(anchor="w", pady=(20, 0))
+    tk.Label(frame, text="The project include our own implementations for the Naive Bayes and ID3 algorithms, "
+                         "and alternative implementations from sklearn library.").pack(anchor="w")
+    teamInfoFrame(frame).pack(anchor="w", pady=(55, 0))
 
     tk.Button(frame, text="Back to Main Menu", font=(None, 12), command=backButton).pack(side="bottom", pady=10)
 
